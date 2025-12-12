d18f28fdac9412cb9bb1f5ab9d1534a1e73a5289
Dinis-17 <dinismedeiros1706@gmail.com>
2025-12-12 08:31:15 +0100

    google urls

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,46 @@
 import './App.css'
-import ReactPlayer from 'react-player'
 
 function App() {
 
+  const videoList = [
+    {
+      id: 1,
+      title: 'All I Want for Christmas Is You',
+      url: 'https://www.youtube.com/embed/aAkMkVFwAoo',
+    },
+    {
+      id: 2,
+      title: 'El Reno Rodolfo',
+      url: 'https://www.youtube.com/embed/uArFYpxDOoU',
+    },
+    {
+      id: 3,
+      title: 'Last christmas',
+      url: 'https://www.youtube.com/watch?v=KhqNTjbQ71A'
+    }
+  ];
+
   return (
-    <>
-      <ReactPlayer width="520px" height="280px" src='https://www.youtube.com/watch?v=cbgCFy8YR7E' controls/>
-    </>
+    <div className="App">
+      <h2>Projecte Nadal</h2>
+
+      {videoList.map((video) => (
+        <div key={video.id} className="video-container">
+          <h3>{video.title}</h3>
+
+          <iframe
+            width="400"
+            height="220"
+            src={video.url}
+            title={video.title}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
+      ))}
+    </div>
   )
 }
 
