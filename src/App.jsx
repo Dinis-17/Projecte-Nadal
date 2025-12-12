80db0e25d9da3acb710e89c830ab80e86bf515f3
Dinis-17 <dinismedeiros1706@gmail.com>
2025-12-12 08:51:55 +0100

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 
 function App() {
-
   const videoList = [
     {
       id: 1,
@@ -15,31 +14,81 @@ function App() {
     },
     {
       id: 3,
-      title: 'Last christmas',
-      url: 'https://www.youtube.com/watch?v=KhqNTjbQ71A'
-    }
+      title: 'Last Christmas',
+      url: 'https://www.youtube.com/embed/KhqNTjbQ71A',
+    },
+    {
+      id: 4,
+      title: 'Burrito Sabanero',
+      url: 'https://www.youtube.com/embed/cbgCFy8YR7E',
+    },
+    {
+      id: 5,
+      title: 'Los Diozes',
+      url: 'https://www.youtube.com/embed/3ZMTt5Sh4-8',
+    },
+    {
+      id: 6,
+      title: 'Peces en el río',
+      url: 'https://www.youtube.com/embed/bx6kO3EtmK0',
+    },
+    {
+      id: 7,
+      title: 'Feliz Navidad - Arcangel',
+      url: 'https://www.youtube.com/embed/cqIeWJKTKmc',
+    },
+    {
+      id: 8,
+      title: 'Christmas Indio',
+      url: 'https://www.youtube.com/embed/yqxIiwa_GYw',
+    },
+    {
+      id: 9,
+      title: 'És Nadal a Andorra',
+      url: 'https://www.youtube.com/embed/0pWhj09pOjU',
+    },
+    {
+      id: 10,
+      title: "M'agrada el Nadal",
+      url: 'https://www.youtube.com/embed/9eHgC_YFyWg',
+    },
+    {
+      id: 11,
+      title: 'El Caganer',
+      url: 'https://www.youtube.com/embed/cAGr9Rna5Qc',
+    },
+    {
+      id: 12,
+      title: '25 de Desembre',
+      url: 'https://www.youtube.com/embed/fdXf5-z19Bw',
+    },
+    {
+      id: 13,
+      title: 'Caga Tió Fake',
+      url: 'https://www.youtube.com/embed/cJ0mGazsAkU',
+    },
   ];
 
   return (
     <div className="App">
-      <h2>Projecte Nadal</h2>
+      <h2>Playlist Nadal</h2>
 
-      {videoList.map((video) => (
-        <div key={video.id} className="video-container">
-          <h3>{video.title}</h3>
+      <div className="video-grid-container">
+        {videoList.map((video) => (
+          <div key={video.id} className="video-card">
+            <h3>{video.title}</h3>
 
-          <iframe
-            width="400"
-            height="220"
-            src={video.url}
-            title={video.title}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-          ></iframe>
-        </div>
-      ))}
+            <iframe
+              src={video.url}
+              title={video.title}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
